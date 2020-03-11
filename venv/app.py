5259811e8d340df9298905ffa0ec265679b10ce8
FPC <izgifth@gmail.com>
2020-03-11 19:44:07 -0400

    Code improvement for finding email address

--- venv/app.py
+++ venv/app.py
@@ -6,7 +6,8 @@
     version: python 3.8.1
 """
 
-from typing import List, Set, IO
+from typing import Set, IO
+import re
 
 file_name: str = "mbox.txt"
 emails: Set[str] = set()  # to store the emails
@@ -20,9 +21,10 @@ else:
         for line in path:
             if line.startswith("From:"):  # find specific lines
                 try:
-                    emails.add(line.split(" ")[1].strip("\n"))  # get only the value from the line
-                except IndexError:  # if no data found
-                    print("Corrupted line >> {} << is omitted".format(line.strip('\n')))  # do not include it to the average
+                    match = re.findall(r'[\w.-]+@[\w.-]+', line)  # find the email address
+                    emails.add(match[0])
+                except IndexError:  # no email address found in the line
+                    print("Corrupted line >>> {} <<< is omitted".format(line.strip('\n')))
 
     count: int = len(emails)  # number of emails
-    print(f"The number of unique email adress(es) in {path.name} is {count}")
+    print(f"The number of unique email address in {path.name} is {count}")
